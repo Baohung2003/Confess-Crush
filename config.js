03baf7b60e7f4418556bc6efaaf27142942411ee
Baohung2003 <108706257+Baohung2003@users.noreply.github.com>
2022-07-05 09:41:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Gửi lời xin lỗi từ a iu của bạnn",
+    introTitle: 'Bé iu của a ơiiiiii',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    btnIntro: 'Hihi😚😚',
+    title: 'Sáng nay a có làm bé iu dận😢😢😢',
+    desc: 'Em tha lỗi cho a nhaaaaa😢😢',
+    btnYes: 'Ok tha lỗi cho đóoo😚😚',
+    btnNo: 'Không nha😡😡',
+    question: 'Vậy bé mún a đền bù gì nàaaa🤗🤗🤗',
+    btnReply: 'Gửi cho anh😘',
+    reply: 'Chìu qua dắt tui đi coi phim',
+    mess: 'Dạaaaa🥰. Yêu bạn nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 6h, mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://www.facebook.com/messages/t/100068996172011' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
